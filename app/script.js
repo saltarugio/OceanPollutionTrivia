f0d3560389480fe3584d10c1fd6e254550723b30
Victor Silva Altarugio <134739051+saltarugio@users.noreply.github.com>
2024-08-26 10:42:10 -0300

    Update script.js

--- app/script.js
+++ app/script.js
@@ -41,12 +41,12 @@ continue_btn.onclick = () => {
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuestions(0); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
-    startTimer(15); //calling startTimer function
+    startTimer(30); //calling startTimer function
     startTimerLine(0);
   }
 };
 
-let timeValue = 15;
+let timeValue = 30;
 let que_count = 0;
 let que_numb = 1;
 let userScore = 0;
